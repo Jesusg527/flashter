a7af439761fe76045d4611b9c5b1ed9e650c42d9
Jesusg527 <116061911+Jesusg527@users.noreply.github.com>
2025-05-16 12:13:14 +0300

    changes to css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,22 +24,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <header>
-        <nav className="navigation">
-          <ul className="lista">
-            <li><a href="/hosting" className="hover:underline">Hosting</a></li>
-            <li><a href="/domain" className="hover:underline">Domain</a></li>
-            <li><a href="/features" className="hover:underline">Features</a></li>
-            <li><a href="/support" className="hover:underline">Support</a></li>
-            <li><a href="/community" className="hover:underline">Community</a></li>
-          </ul>
-        </nav>
+          <nav className="navigation">
+            <ul className="lista">
+              <li><a href="/hosting">Hosting</a></li>
+              <li><a href="/domain" >Domain</a></li>
+              <li><a href="/features">Features</a></li>
+              <li><a href="/support">Support</a></li>
+              <li><a href="/community">Community</a></li>
+            </ul>
+          </nav>
         </header>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
         {children}
       </body>
     </html>
   );
 }
+
