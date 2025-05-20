9140319976d876dccad1fe4d0156e185c711d645
Jesusg527 <116061911+Jesusg527@users.noreply.github.com>
2025-05-20 04:47:29 +0300

    corrected header in css

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -3,9 +3,9 @@ import styles from './Header.module.css';
 // components/Header.tsx
 export default function Header() {
   return (
-    <header className={styles.navigation}>
-      <nav>
-        <ul className="lista">
+    <header className={styles.header}>
+      <nav className={styles.navigation}>
+        <ul className={styles.lista}>
           <li><a href="#hosting" className="hover:underline">Hosting</a></li>
           <li><a href="#domain" className="hover:underline">Domain</a></li>
           <li><a href="#features" className="hover:underline">Features</a></li>
